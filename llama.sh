73e05e5c667c202fabd8d419073d99fe8832a2b1
Ivan Uglov <i.uglov@ispsystem.com>
2025-02-20 18:44:28 +0700

    DS 32b offload and other optimizations

--- llama.sh
+++ llama.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 #model_path="./models/llama3.3/Llama-3.3-70B-Instruct-Q4_K_L.gguf"
-model_path="./models/deepseek/DeepSeek-R1-Distill-Qwen-32B-Q4_K_L.gguf" #36: 8,22,22
+model_path="./models/deepseek/DeepSeek-R1-Distill-Qwen-32B-Q4_K_L.gguf" #61: 130,435,435: 131072,q8_0,q8_0
 #model_path="./models/deepseek/DeepSeek-R1-Distill-Qwen-14B-Q8_0.gguf"  #48: 8,22,22
 
 host="HOST_IP"
 port="8080"
-layers="48" #"32"
-split="8,22,22" #"1,5,5"
-ctx_size="98304" #"65536" #"65536"
-parseq="2"
+layers="61" #"32"
+split="130,435,435" #"8,22,22" #"1,5,5"
+ctx_size="131072" #"98304" #"65536" #"65536" #"131072"
+parseq="1"
 
+# draft parameters
 ./llama_bins/llama-server --model ${model_path} \
 --port ${port} --host ${host} \
 -ngl ${layers} \
---ctx-size ${ctx_size} \
+--ctx-size ${ctx_size} -ctk q8_0 -ctv q8_0 \
 -ts ${split} \
 -fa -nkvo \
 --log-colors --verbose \
 -cb -np ${parseq} \
+--no-warmup \
+--prio 3 --threads 8 \
+--log-file llama-server.log --log-timestamps \
 2>server_err.log 1>server_out.log&
+
